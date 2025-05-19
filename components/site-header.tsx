2960e6784aa0e1c6e63dea90293a06ae84835e9a
gopika-gopal <140744560+gopika-gopal@users.noreply.github.com>
2025-05-19 13:14:03 +0530

    changes

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -2,7 +2,7 @@
 export function SiteHeader() {
    return (
     <header className="flex h-[--header-height] shrink-0 items-center justify-center pb-2">
-      <h1 className="text-4xl font-bold mt-10 mb-4">Articles</h1>
+      <h1 className="text-3xl font-bold mt-10 mb-4">Articles</h1>
     </header>
   );
 }
